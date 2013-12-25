c923483c06ee966926acf7106583b1233e78cd09
lobsteropteryx <ian.firkin@gmail.com>
2013-12-25 16:53:46 -0600

    Fixed Jasmine and WebStorm integration, so that tests run properly now.  Wrote first test for CC module.

--- www/js/CoordinateConverter.js
+++ www/js/CoordinateConverter.js
@@ -1,3 +1,13 @@
 /**
  * Created by Ian on 12/22/13.
  */
+
+var CoordinateConverter = (function () {
+    var my = {};
+
+    my.project = function(point) {
+        return {Easting:0, Northing:0, Zone:0};
+    }
+
+    return my;
+}());
--- www/spec/CoordinateConverterSpec.js
+++ www/spec/CoordinateConverterSpec.js
@@ -2,10 +2,10 @@
  * Created by Ian on 12/22/13.
  */
 
-describe( "projection library", function () {
+describe( "Coordinate Converter", function () {
     describe( "wgs84 lat/lon to nad27 UTM converter", function () {
         it("Takes an input tuple and returns a triple", function() {
-            expect(CoordinateConverter.project({Lon:0, Lat:0}).toEqual({Easting:0, Northing:0, Zone:0}));
+            expect(CoordinateConverter.project({Lon:0, Lat:0})).toEqual({Easting:0, Northing:0, Zone:0});
         })
     });
 });
