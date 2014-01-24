87ca0eebad467f4547bfc9ebdc34c930a2b74d6e
lobsteropteryx <ian.firkin@gmail.com>
2014-01-23 22:33:21 -0600

    Added proper messages for unaddressed and placed

--- www/js/models/CurrentPosition.js
+++ www/js/models/CurrentPosition.js
@@ -24,7 +24,36 @@
                DistanceOutside: 0,
                Site: { quad: '', site_id: ''}
            },
-           message: 'Test message'
-       }
+           message: ''
+       },
+
+        initialize: function() {
+           this.listenTo(this, 'change:nearestSite', this.updateMessage);
+        },
+
+        updateMessage: function() {
+            var nearestSite = this.get('nearestSite');
+            var message;
+            if (typeof nearestSite === 'undefined') {
+                message = 'No sites loaded, or wrong UTM zone';
+            } else if (typeof nearestSite.Site.xact === 'undefined') {
+                message = 'No trap at this site';
+            } else if (typeof nearestSite.Site.visit === 'undefined') {
+                var traptype = nearestSite.Site.trap_type;
+                var date = this.formatDate(nearestSite.Site.txn_date);
+                message = traptype + ' trap placed here on ' + date;
+            } else {
+                message = 'Invalid site';
+            }
+
+            this.set('message', message);
+        },
+
+        formatDate: function(dateString) {
+            //"2013-02-06T00:00:00-00:00"
+            var parts = [];
+            parts = dateString.split('-');
+            return parts[1] + '/' + parts[2].substring(0,2) + '/' + parts[0].substring(2,4);
+        }
     });
 })();
--- www/spec/javascripts/models/CurrentPositionSpec.js
+++ www/spec/javascripts/models/CurrentPositionSpec.js
@@ -6,5 +6,78 @@ describe("Tests for CurrentPosition", function() {
    it("Can be created", function() {
        var p = new app.models.CurrentPosition();
        expect(p).toBeDefined();
-   })
+   });
+
+    describe("Changes the message when the nearest site changes", function() {
+        var position = new app.models.CurrentPosition();
+
+
+
+        var expectMessageToMatchSite = function(site, expectedMessage) {
+            var siteHeader = {
+                Distance: '10',
+                Found: false,
+                Bearing: 'N',
+                DistanceOutside: 0
+            };
+            siteHeader.Site = site;
+            position.set({nearestSite: siteHeader});
+            var message = position.get('message');
+            expect(message).toEqual(expectedMessage);
+        };
+
+        it ("Formats the date correctly", function() {
+            var formattedDate = position.formatDate("2013-02-06T00:00:00-00:00");
+            expect(formattedDate).toEqual('02/06/13');
+        });
+
+        it ("Displays unaddressed message", function() {
+
+            var unaddressed = {
+                "zone":15,
+                "xth":"329229",
+                "yth":"3475979",
+                "quad":"FIREP",
+                "site_id":1,
+                "grid":"30",
+                "trap_type":"Milk Carton",
+                "moth_count":0
+            };
+            expectMessageToMatchSite(unaddressed, 'No trap at this site');
+        });
+
+        it ("Displays placed delta message", function() {
+            var delta = {
+                "zone":17,
+                "xth":"700028",
+                "yth":"4141028",
+                "xact":"700028",
+                "yact":"4141028",
+                "quad":"HOLID",
+                "site_id":9009,
+                "grid":"9999",
+                "trap_type":"Delta",
+                "moth_count":0,
+                "txn_date":"2013-02-06T00:00:00-00:00"
+            };
+            expectMessageToMatchSite(delta, 'Delta trap placed here on 02/06/13');
+        });
+
+        it ("Displays placed milk carton message", function() {
+            var milkCarton = {
+                "zone":17,
+                "xth":"700028",
+                "yth":"4141028",
+                "xact":"700028",
+                "yact":"4141028",
+                "quad":"HOLID",
+                "site_id":9009,
+                "grid":"9999",
+                "trap_type":"Milk Carton",
+                "moth_count":0,
+                "txn_date":"2013-02-06T00:00:00-00:00"
+            };
+            expectMessageToMatchSite(milkCarton, 'Milk Carton trap placed here on 02/06/13');
+        });
+    });
 });
