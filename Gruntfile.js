d7dd116a7140d82ef23592847d06600534c8232b
lobsteropteryx <ian.firkin@gmail.com>
2014-01-24 22:28:20 -0600

    Added Travis CI setup and grunt

---
+++ Gruntfile.js
@@ -0,0 +1,31 @@
+/**
+ * Created by Ian on 1/24/14.
+ */
+module.exports = function(grunt) {
+
+    // Project configuration.
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        jasmine: {
+            components: {
+                src: [
+                    'www/*js'
+                ],
+                options: {
+                    specs: 'www/spec/javascripts/*Spec.js',
+                    keepRunner : true
+                    //helpers: 'test/spec/*.js'
+                }
+            }
+        }
+    });
+
+    // Load the plugin that provides the task.
+    grunt.loadNpmTasks('grunt-contrib-jasmine');
+
+    // Default task(s).
+    grunt.registerTask('travis', [
+        /*'jshint',*/'jasmine'
+    ]);
+
+};
