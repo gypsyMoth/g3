bcb469f3b6222232771cb899838f4b71e22a0362
lobsteropteryx <ian.firkin@gmail.com>
2014-01-24 22:50:11 -0600

    Added local grunt install for Travis CI

--- platforms/android/assets/www/spec/javascripts/util/DBSpec.js
+++ platforms/android/assets/www/spec/javascripts/util/DBSpec.js
@@ -2,37 +2,40 @@
 
 describe("DB Module", function() {
 
-    describe("Init", function() {
-        app.db.initialize().then(
-            describe("After initialization", function() {
-                it("Has filesystem and root defined", function(done) {
-                    app.db.initialize().then( function() {
-                    expect(app.Filesystem).toBeDefined();
-                    expect(app.Root).toBeDefined();
-               },
-               function(error){
-                console.log(expect());
-                expect("initialize received error: " + error).toFail();
-               })
-               .always(done);
-                });
+    app.db.initialize().then(
+        describe("After initialization", function() {
+            it("Has filesystem and root defined", function(done) {
+                app.db.initialize().then( function() {
+                expect(app.Filesystem).toBeDefined();
+                expect(app.Root).toBeDefined();
+           },
+           function(error){
+            console.log(expect());
+            expect("initialize received error: " + error).toFail();
+           })
+           .always(done);
+            });
 
-                it("Can check for files in local storage", function(done) {
-                    app.db.countFiles().then( function() {
-                            expect(app.fileCount).toBeDefined();
-                        },
-                        function(error){
-                            console.log(expect());
-                            expect("file count received error: " + error).toFail();
-                        })
-                        .always(done);
-                });
+            it("Can check for files in local storage", function(done) {
+                app.db.countFiles().then( function() {
+                        expect(app.fileCount).toBeDefined();
+                    },
+                    function(error){
+                        console.log(expect());
+                        expect("file count received error: " + error).toFail();
+                    })
+                    .always(done);
+            });
 
-
-                it("has a method to save sites to local storage", function() {
+            describe("Saving sites to the local filesystem", function() {
+                it("Has a method to save sites to local storage", function() {
                     expect(app.db.saveSites).toBeDefined();
                 });
-            })
-        );
-    });
+
+                it("Keeps track of the currently loaded sites file", function() {
+
+                });
+            });
+        })
+    );
 });
