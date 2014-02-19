2d2eef6ad0a2556791f3e8aa621dd5a2be38dfb2
lobsteropteryx <ian.firkin@gmail.com>
2014-02-19 11:46:18 -0600

    removed local.properties files

--- platforms/android/gen/R.java.d
+++
@@ -1,9 +0,0 @@
-/Users/ianfirkin/Documents/GitHub/g3/platforms/android/gen/com/phonegap/g3/R.java \
- : /Users/ianfirkin/Documents/GitHub/g3/platforms/android/res/drawable/icon.png \
-/Users/ianfirkin/Documents/GitHub/g3/platforms/android/res/drawable-hdpi/icon.png \
-/Users/ianfirkin/Documents/GitHub/g3/platforms/android/res/drawable-ldpi/icon.png \
-/Users/ianfirkin/Documents/GitHub/g3/platforms/android/res/drawable-mdpi/icon.png \
-/Users/ianfirkin/Documents/GitHub/g3/platforms/android/res/drawable-xhdpi/icon.png \
-/Users/ianfirkin/Documents/GitHub/g3/platforms/android/res/values/strings.xml \
-/Users/ianfirkin/Documents/GitHub/g3/platforms/android/res/xml/config.xml \
-/Users/ianfirkin/Documents/GitHub/g3/platforms/android/bin/AndroidManifest.xml \
