15ec24557852302db01994a12ee0bdfaeaedc19f
lobsteropteryx <ian.firkin@gmail.com>
2014-02-19 13:41:21 -0600

    removing android output

--- platforms/android/assets/www/spec/javascripts/util/EncoderSpec.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Ian on 1/28/14.
- */
-describe("Encoder Module", function() {
-   it("Has a method that returns an encoded string", function() {
-      expect(app.encoder.getString).toBeDefined();
-   });
-});
